b1db6c600790da055506ca72baae8950477edf6d
Caleb Hoff <crunchycat1@gmail.com>
2024-01-02 22:21:07 -0600

    mangle properties in @solid-js/router

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default ({ mode }: { mode: 'production' | 'development' | 'test' }) => {
           ? {
               compress: { arguments: true, ecma: 2020, hoist_funs: true, passes: 3, pure_getters: true, unsafe: true, unsafe_arrows: true, unsafe_comps: true, unsafe_symbols:true }, //prettier-ignore
               format: { comments: false, ecma: 2020, wrap_func_args: false },
-              mangle: { properties: { regex: /^(?:observers|observerSlots|comparator|updatedAt|owned|route|score|when|sourceSlots|fn|cleanups|owner|pure|suspense|inFallback|isRouting|beforeLeave|Provider|outlet|utils|resolvePath|parsePath|renderPath|originalPath|effects|tState|disposed|sensitivity|navigatorFactory|keyed)$/ } }, //prettier-ignore
+              mangle: { properties: { regex: /^(?:observers|observerSlots|comparator|updatedAt|owned|route|score|when|sourceSlots|fn|cleanups|owner|pure|suspense|inFallback|isRouting|beforeLeave|Provider|preloadRoute|outlet|utils|explicitLinks|actionBase|resolvePath|branches|routerState|parsePath|renderPath|originalPath|effects|tState|disposed|sensitivity|navigatorFactory|keyed)$/ } }, //prettier-ignore
             }
           : undefined,
       modulePreload: { polyfill: false }, // Delete this line if outputting more than 1 chunk
