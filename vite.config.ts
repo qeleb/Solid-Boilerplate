95ed3666cc040e51111c8abcc5754c2586b7b6ea
Caleb Hoff <crunchycat1@gmail.com>
2024-04-15 16:32:36 -0500

    update deps & refactor vite config

--- vite.config.ts
+++ vite.config.ts
@@ -17,11 +17,11 @@ export default ({ mode }: { mode: 'production' | 'development' | 'test' }) => {
   return defineConfig({
     envDir: 'env',
     build: {
+      target: 'es2020',
       rollupOptions: {
         output: { entryFileNames: '[hash:6].js', chunkFileNames: '[hash:6].js', assetFileNames: '[hash:6][extname]' },
         treeshake: { tryCatchDeoptimization: false },
       },
-      target: 'es2020',
       minify: 'terser',
       cssMinify: 'lightningcss',
       terserOptions: {
