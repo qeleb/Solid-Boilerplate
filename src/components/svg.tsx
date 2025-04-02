c46dc647dc9e23062e427fc046f51a7d5cd315fc
Caleb Hoff <crunchycat1@gmail.com>
2025-04-01 19:35:51 -0500

    allow assigning props to SVGs

--- src/components/svg.tsx
+++ src/components/svg.tsx
@@ -2,8 +2,14 @@ import iconGithub from '@/assets/github.svg';
 import iconMenu from '@/assets/menu.svg';
 
 /* SVG Component */
-const createSVG = (innerHTML: string) => Object.assign(document.createElement('template'), { innerHTML }).content;
+const createSVG =
+  (innerHTML: string) =>
+  (props?: DeepPartial<SVGElement>): SVGElement =>
+    Object.assign(
+      Object.assign(document.createElement('template'), { innerHTML }).content.firstChild as SVGElement,
+      props
+    );
 
 /* Icons */
-export const IconGithub = () => createSVG(iconGithub);
-export const IconMenu = () => createSVG(iconMenu);
+export const IconGithub = createSVG(iconGithub);
+export const IconMenu = createSVG(iconMenu);
--- src/global.d.ts
+++ src/global.d.ts
@@ -12,6 +12,18 @@ interface ImportMeta {
 }
 
 /* Utilities */
+type Nullable<T> = { [K in keyof T]: T[K] | null };
+
+type DeepKeys<T extends object> = {
+  [K in keyof T]: T[K] extends object & { length?: never } ? DeepKeys<T[K]> | never : K;
+}[keyof T];
+
+type DeepPartial<T> = T extends object
+  ? {
+      [P in keyof T]?: DeepPartial<T[P]>;
+    }
+  : T;
+
 type SolidEvent<C, T = Element> = Event & { currentTarget: C; target: T };
 type SolidClipboardEvent<C, T = Element> = ClipboardEvent & { currentTarget: C; target: T };
 type SolidFocusEvent<C, T = Element> = FocusEvent & { currentTarget: C; target: T };
