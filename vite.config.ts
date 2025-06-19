cfdd5fbc742885ce1fcf3ade4a76a1180970ba1c
Caleb Hoff <crunchycat1@gmail.com>
2025-06-19 13:37:30 -0500

    +20B ): but improved compiler correctness & simplicity

--- vite.config.ts
+++ vite.config.ts
@@ -33,7 +33,7 @@ export default ({ mode }: { mode: 'production' | 'development' | 'test' }) => {
       terserOptions: {
         ecma: 2020,
         compress: { arguments: true, hoist_funs: true, keep_fargs: false, passes: 3, unsafe: true, unsafe_arrows: true, unsafe_comps: true, unsafe_proto: true }, //prettier-ignore
-        format: { comments: false, wrap_func_args: false },
+        format: { comments: false },
         mangle: { properties: { regex: /^(?:observers|observerSlots|comparator|updatedAt|owned|route|score|when|sourceSlots|fn|cleanups|owner|pure|suspense|inFallback|isRouting|beforeLeave|Provider|preloadRoute|outlet|utils|explicitLinks|actionBase|resolvePath|branches|routerState|parsePath|renderPath|originalPath|effects|tState|disposed|sensitivity|navigatorFactory|keyed|intent)$/ } }, //prettier-ignore
       },
       modulePreload: { polyfill: false }, // Delete this line if outputting more than 1 chunk
