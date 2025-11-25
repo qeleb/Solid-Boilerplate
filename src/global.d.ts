df328208fc21147fd41cd81ec386e517a4e601a9
Caleb Hoff <crunchycat1@gmail.com>
2025-11-25 12:24:06 -0600

    update deps & add 1 global event type

--- src/global.d.ts
+++ src/global.d.ts
@@ -28,4 +28,5 @@ type SolidEvent<C, T = Element> = Event & { currentTarget: C; target: T };
 type SolidClipboardEvent<C, T = Element> = ClipboardEvent & { currentTarget: C; target: T };
 type SolidFocusEvent<C, T = Element> = FocusEvent & { currentTarget: C; target: T };
 type SolidKeyboardEvent<C, T = Element> = KeyboardEvent & { currentTarget: C; target: T };
+type SolidPointerEvent<C, T = Element> = PointerEvent & { currentTarget: C; target: T };
 type SolidMouseEvent<C, T = Element> = MouseEvent & { currentTarget: C; target: T };
